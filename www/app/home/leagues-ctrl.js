3ffbad634a28fe5094965883d6c56434ccec5f0f
Yuichi Hagio <yhagio87@gmail.com>
2014-10-12 16:55:59 -0400

    added locations-ctrl and edited leagues-ctrl, getting data wont work somehow

--- www/app/home/leagues-ctrl.js
+++ www/app/home/leagues-ctrl.js
@@ -2,16 +2,22 @@
   'use strict';
 
   angular.module('eliteApp')
-    .controller('leaguesCtrl', ['eliteApi', leaguesCtrl]);
+    .controller('leaguesCtrl', ['$state', 'eliteApi', leaguesCtrl]);
 
-      function leaguesCtrl(eliteApi){
+      function leaguesCtrl($state, eliteApi){
         console.log('Hola');
         var vm = this;
 
-        var leagues = eliteApi.getLeagues;
-        var leagueData = eliteApi.getLeagueData;
+        var leagues = eliteApi.getLeagues();
+        vm.leagues = leagues;
 
         console.log('leagues', leagues);
-        console.log('leagueData', leagueData);
+        console.log('leagueData', eliteApi.leagueData());
+
+        vm.selectLeague = function(leagueId){
+          // Todo: select correct league
+          $state.go('app.teams');
+        };
+
       };
 })();
---
+++ www/app/locations/locations-ctrl.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular.module('eliteApp')
+    .controller('locationsCtrl', ['eliteApi', locationsCtrl]);
+
+      function locationsCtrl(eliteApi){
+        var vm = this;
+        var data = eliteApi.getLeagueData();
+        vm.locations = data.locations;
+      };
+})();
